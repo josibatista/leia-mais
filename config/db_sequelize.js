ca8b87381bf08db02a28e30d415da540bf5b77d8
pamelaBertiBraz <pbraz@alunos.utfpr.edu.br>
2026-04-21 16:27:09 -0300

    feat: Adição de configuração do banco de dados Supabase

---
+++ config/db_sequelize.js
@@ -0,0 +1,23 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DB_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    logging: false,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
+});
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+// Models
+db.usuario = require('../models/usuario')(sequelize, Sequelize);
+
+module.exports = db;
