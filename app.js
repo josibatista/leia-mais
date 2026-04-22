c5319399720b1b5146abd3660388bba498ca47df
pamelaBertiBraz <pbraz@alunos.utfpr.edu.br>
2026-04-22 18:17:46 -0300

    feat: Adição de app.js ao projeto

---
+++ app.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const db = require('./config/db_sequelize');
+const routes = require('./routes/route');
+
+const app = express();
+
+db.sequelize.sync()
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+app.listen(8080, function(){
+  console.log("Servidor no http://localhost:8080");
+})
