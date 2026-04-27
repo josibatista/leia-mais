feca0732c1ce8c65baa9dfcfe2642ed6d4101f91
Nadia Yuzawa <nadiayuzawa@gmail.com>
2026-04-27 10:51:46 -0300

    feat: Adição método PUT Livro

--- controllers/livroController.js
+++ controllers/livroController.js
@@ -1,14 +1,27 @@
+const { Op } = require('sequelize');
 const db = require('../config/db_sequelize');
 
 module.exports = {
     async postLivro(req, res) {
         try {
-           const { titulo, descricao, editora, anoPublicacao, genero, imagemCapa } = req.body;
+           let { titulo, descricao, editora, anoPublicacao, genero, imagemCapa } = req.body;
+
+           titulo = titulo?.trim();
+           descricao = descricao?.trim();
+           editora = editora?.trim();
+           genero = genero?.trim();
+           imagemCapa = imagemCapa?.trim();
 
            if (!titulo) {
             return res.status(422).json({error: 'O campo título é obrigatório'});
            } else {
-            const tituloExistente = await db.Livro.findOne({ where: {titulo } })
+            const tituloExistente = await db.Livro.findOne({ 
+                where: { 
+                    titulo: { 
+                        [Op.iLike]: titulo 
+                    } 
+                } 
+            });
              if (tituloExistente) {
                 return res.status(400).json({ error: 'Título já cadastrado' });
              }
@@ -36,5 +49,68 @@ module.exports = {
             console.error(error);
             res.status(500).json({ error: 'Erro ao criar livro' });
         }
+    },
+    async putLivro(req, res) {
+        try {
+            const { id } = req.params;
+            let { titulo, descricao, anoPublicacao, editora, genero, imagemCapa } = req.body;
+
+            titulo = titulo?.trim();
+            descricao = descricao?.trim();
+            editora = editora?.trim();
+            genero = genero?.trim();
+            imagemCapa = imagemCapa?.trim();
+
+            const idExistente = await db.Livro.findOne({ where: { id } });
+            if (!idExistente) {
+                return res.status(404).json({ error: 'Livro não encontrado' });
+            }
+
+            if (titulo === '') {
+                return res.status(422).json({ error: 'O campo título não pode ser vazio' });
+            }
+
+            if (titulo !== undefined && titulo !== idExistente.titulo.trim()) {
+                const tituloExistente = await db.Livro.findOne({ 
+                    where: { 
+                        titulo: { 
+                            [Op.iLike]: titulo 
+                        },
+                        id: { 
+                            [Op.ne]: id 
+                        }
+                    } 
+                });
+                if (tituloExistente) {
+                    return res.status(400).json({ error: 'Título já cadastrado' });
+                }
+            }
+
+            if (genero === '') {
+                return res.status(422).json({ error: 'O campo gênero não pode ser vazio' });
+            }
+
+            if (editora === '') {
+                return res.status(422).json({ error: 'O campo editora não pode ser vazio' });
+            }
+
+            await db.Livro.update({ 
+                titulo, 
+                descricao, 
+                editora,
+                anoPublicacao, 
+                genero, 
+                imagemCapa 
+            },{ 
+                where: { id } 
+            });
+
+            const livroAtualizado = await db.Livro.findOne({ where: { id } });
+            res.status(200).json(livroAtualizado);
+
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Erro ao atualizar livro' });
+        }
     }
-}        
+}
--- routes/route.js
+++ routes/route.js
@@ -4,5 +4,6 @@ const livroController = require('../controllers/livroController');
 const router = express.Router();
 
 router.post('/livros', livroController.postLivro);
+router.put('/livros/:id', livroController.putLivro);
 
 module.exports = router;
