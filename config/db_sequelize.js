8c7c6a2ed34e02bb69e801db95c0d1ce630509fd
Nadia Yuzawa <nadiayuzawa@gmail.com>
2026-04-26 23:21:22 -0300

    feat: conexão + relacionamento entre tabelas

---
+++ config/db_sequelize.js
@@ -0,0 +1,35 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DB_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    logging: false,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
+});
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.Livro = require('../models/livro')(sequelize, Sequelize);
+db.Autor = require('../models/autor')(sequelize, Sequelize);
+db.LivroAutor = require('../models/livroAutor')(sequelize, Sequelize)
+
+db.Livro.belongsToMany(
+  db.Autor, { 
+    through: db.LivroAutor, 
+    foreignKey: 'livroId' 
+  });
+db.Autor.belongsToMany(
+  db.Livro, {
+     through: db.LivroAutor,
+     foreignKey: 'autorId' 
+    });
+
+module.exports = db;
